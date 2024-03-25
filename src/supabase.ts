cf413315698c1d85a2a47d01d1ff7b1b91aa156b
Thomas Wallsmith <thomaswallsmith@gmail.com>
2024-03-25 19:37:35 -0400

    fix: new service token

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,5 +3,5 @@ import type { Database } from './database.types';
 
 export const supabase = createClient<Database>(
     'https://okawhzshfmfwqkqtqinh.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rYXdoenNoZm1md3FrcXRxaW5oIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTA3MTcxOSwiZXhwIjoyMDI0NjQ3NzE5fQ.q8h6Yi8NDo4Mg2IyYG42KGIoTxKw7LxQH8s1dQml6DU'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rYXdoenNoZm1md3FrcXRxaW5oIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTQwODQ1NywiZXhwIjoyMDI2OTg0NDU3fQ.DReHfW_fvKgZuhI5R-fP3b0lfQnL3J5n-XIDsBSrE_Y'
 );
