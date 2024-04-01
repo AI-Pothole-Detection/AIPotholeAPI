6d6af9c5af47913201d0354d6d8bf895d0acfafa
twallie <thomaswallsmith@gmail.com>
2024-03-31 22:44:08 -0400

    fix: small bug in alerting

--- src/advanced-alerting.ts
+++ src/advanced-alerting.ts
@@ -1,9 +1,9 @@
-import { ALERT_THRESHOLD_METERS } from "./constants";
-import { getClosestPothole } from "./rpcs";
+import { ALERT_THRESHOLD_METERS } from './constants';
+import { getClosestPothole } from './rpcs';
 
 export async function shouldAlert(long: number, lat: number): Promise<boolean> {
     const closest = await getClosestPothole(lat, long);
-    if (closest === undefined) {
+    if (closest === undefined || closest === null) {
         return false;
     }
     return closest.distance <= ALERT_THRESHOLD_METERS;
